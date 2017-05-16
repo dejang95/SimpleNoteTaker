35d85ad1d0d8fb34629e3cf46603ff63adabeb61
Dejan Gogic <dejangog@outlook.com>
2017-05-16 13:27:35 +0200

    Fixed Day/Night Mode and other options.

--- app/src/main/java/team4/hci/simplenotetaker/ActivityMode.java
+++ app/src/main/java/team4/hci/simplenotetaker/ActivityMode.java
@@ -2,11 +2,8 @@ package team4.hci.simplenotetaker;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
