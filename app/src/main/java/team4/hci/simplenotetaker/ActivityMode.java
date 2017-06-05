12e2c71a232beeae3a14ccb3f0ec91e4c05f011a
Vadim <vadim.zama@hotmail.com>
2017-06-05 14:17:43 +0200

    new icons

--- app/src/main/java/team4/hci/simplenotetaker/ActivityMode.java
+++ app/src/main/java/team4/hci/simplenotetaker/ActivityMode.java
@@ -1,11 +1,16 @@
 package team4.hci.simplenotetaker;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
+import android.text.Selection;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 // This class is for the user so he can choose between the Day/Night/Auto Mode
 
@@ -15,11 +20,12 @@ public class ActivityMode extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode);
+
         //Default Night Mode as Auto Mode
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
         btnAutoMode= (Button) findViewById(R.id.buttonAuto);
         btnNightMode= (Button) findViewById(R.id.buttonNight);
-        btnDayMode= (Button) findViewById(R.id.buttonDay);
+        btnDayMode= (Button) findViewById(R.id.buttonAuto);
 
 
         btnAutoMode.setOnClickListener(new View.OnClickListener() {
@@ -37,8 +43,15 @@ public class ActivityMode extends AppCompatActivity {
             public void onClick(View v) {
                 // Set Default Night Mode
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                Intent i=new Intent(ActivityMode.this,MainActivity.class);
+               Intent i=new Intent(ActivityMode.this,MainActivity.class);
+
                 startActivity(i);
+
+
+
+
+
+
             }
         });
 
--- app/src/main/java/team4/hci/simplenotetaker/MainActivity.java
+++ app/src/main/java/team4/hci/simplenotetaker/MainActivity.java
@@ -22,7 +22,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+
 
 
 
